5c3dd40a96fffa5991dc3fa0d27c4700b844089d
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-01-26 15:29:25 +0100

    inserimento categoria nella show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,5 +4,12 @@
     <div class="text-center">
         <h1>{{ $singolo_post->title }}</h1>
         <p>{{ $singolo_post->body }}</p>
+
+        @if ($singolo_post->category)
+           Categoria: {{ $singolo_post->category['name'] }}
+        @else
+        Categoria: null
+        @endif
+
     </div>
 @endsection
